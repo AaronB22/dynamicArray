db12e2253ce3d69a2cef426338020f327fb61c3d
JHud <219741840+JamHudson@users.noreply.github.com>
2026-04-23 11:56:10 -0700

    tests for set()

--- src/AmazingListTest.java
+++ src/AmazingListTest.java
@@ -13,4 +13,41 @@ public class AmazingListTest {
 
         assertEquals(2,actual);
     }
+
+    @Test
+    void testListDefaultSize() {
+        AmazingList list = new AmazingList();
+
+        int actual = list.capacity();
+    
+        assertEquals(8,actual);
+    }
+
+    @Test
+    void testListSetIndexMatches() {
+        AmazingList list = new AmazingList(4);
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.set(2,"d");
+
+        String actual = list.get(2);
+
+        assertEquals("d", actual);
+    }
+
+    @Test
+    void testListSetNoValuesLost() {
+        AmazingList list = new AmazingList(4);
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.set(2,"d");
+
+        String actual = list.get(3);
+
+        assertEquals("c", actual);
+    }
 }
