41b3cd0d20e47fe1f08a26c4f6702654155c060f
AaronB22 <aaronthesnowboarder@live.com>
2026-04-23 11:25:26 -0700

    added constructor

--- src/AmazingList.java
+++ src/AmazingList.java
@@ -1,3 +1,11 @@
 public class AmazingList implements StringList {
+    private String[] array;
+    public AmazingList() {
+        this(8);
+    }
+
+    public AmazingList(int listSize){
+        this.array= new String[listSize];
+    }
     
 }
