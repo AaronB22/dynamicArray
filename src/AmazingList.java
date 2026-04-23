8dcc816362da5aec2e4949020af4be090bdc3349
JHud <219741840+JamHudson@users.noreply.github.com>
2026-04-23 11:29:31 -0700

    Added signatures and add method

--- src/AmazingList.java
+++ src/AmazingList.java
@@ -1,11 +1,42 @@
 public class AmazingList implements StringList {
+
     private String[] array;
+    private int valueCount = 0;
+
     public AmazingList() {
         this(8);
     }
 
     public AmazingList(int listSize){
-        this.array= new String[listSize];
+        this.array = new String[listSize];
     }
     
+    public String get(int index) {
+        return "";
+    }
+
+    public void set(int index, String value) {
+
+    }
+
+    public void add(String value) {
+        array[valueCount] = value;
+        valueCount++;
+        if (size() == capacity()) {
+            // Expand static array
+        }
+    }
+
+    public String remove(int index) {
+        return "";
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public int capacity() {
+        return 0;
+    }
+
 }
